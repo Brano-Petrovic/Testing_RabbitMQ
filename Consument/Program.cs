28cb60da93676f440e6c95bf1762037165bf3a92
Brano-Petrovic <68059369+Brano-Petrovic@users.noreply.github.com>
2020-11-15 18:52:35 +0100

    Initial commit

---
+++ Consument/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Threading;
+using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Consument
+{
+    class Program
+    {
+        static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var conn = factory.CreateConnection();
+            var channel = conn.CreateModel();
+
+            channel.QueueDeclare(queue: "testQueue", durable: true, exclusive: false, autoDelete: false);
+
+            var consumer = new EventingBasicConsumer(channel);
+            var helpList = new List<string>();
+            consumer.Received += (s, e) =>
+            {
+                //consume
+                //var msg = Encoding.UTF8.GetString(e.Body.ToArray());
+                //Console.WriteLine(msg);
+                //channel.BasicAck(e.DeliveryTag, false);
+
+                //Thread.Sleep(800);
+
+                var msg = Encoding.UTF8.GetString(e.Body.ToArray());
+                helpList.Add(msg);
+                channel.BasicAck(e.DeliveryTag, false);
+
+                if (helpList.Count == 5)
+                {
+                    foreach (var i in helpList)
+                        Console.WriteLine(i);
+                    helpList.Clear();
+                }
+
+                Thread.Sleep(800);
+            };
+            channel.BasicConsume("testQueue", false, consumer);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Testing_RabbitMQ/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using System.Threading;
+using System;
+using System.Text;
+
+namespace Testing_RabbitMQ
+{
+    class Program
+    {
+        static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("testExchange", type: ExchangeType.Fanout, durable: true, autoDelete: false) ;
+            channel.QueueDeclare(queue: "testQueue",durable: true, exclusive: false, autoDelete: false);
+            channel.QueueBind("testQueue", "testExchange", string.Empty);
+
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+            int count = 0;
+            var number = new Random();
+
+            while (true)
+            {
+                count++;
+                
+                var msg = $"Testing message number:{count}. Value {number.Next(-100, 100)}.";
+                Console.WriteLine(msg);
+                
+                channel.BasicPublish("testExchange", 
+                    string.Empty, 
+                    properties,
+                    Encoding.UTF8.GetBytes(msg)
+                    );
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
